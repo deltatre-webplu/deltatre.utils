8db4dde62a810b6f2d901f26a62c92e088c7e01b
Giovanni Frigione <giovanni.frigione@deltatre.com>
2018-04-13 12:37:18 +0200

    Implemented foreach and isnullorempty

---
+++ src/Deltatre.Utils/Extensions/Enumerable/EnumerableExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Deltatre.Utils.Extensions.Enumerable
+{
+    public static class EnumerableExtensions
+    {
+      public static bool IsNullOrEmpty<T>(this IEnumerable<T> source)
+      {
+        if (source == null)
+          return true;
+
+        return !source.Any();
+      }
+
+      public static void ForEach<T>(this IEnumerable<T> source, Action<T> toBeDone)
+      {
+        if (source == null)
+          throw new ArgumentNullException(nameof(source));
+
+        if (toBeDone == null)
+          throw new ArgumentNullException(nameof(toBeDone));
+
+        foreach (var item in source)
+          toBeDone(item);
+      }
+    }
+}
---
+++ tests/Deltatre.Utils.Tests/Extensions/Enumerable/EnumerableExtensionsTest.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using Deltatre.Utils.Extensions.Enumerable;
+using NUnit.Framework;
+using Linq = System.Linq;
+
+namespace Deltatre.Utils.Tests.Extensions.Enumerable
+{
+  [TestFixture]
+  public class EnumerableExtensionsTest
+  {
+    [Test]
+    public void IsNullOrEmpty_Should_Return_True_When_Source_Is_Null()
+    {
+      // ACT
+      var result = EnumerableExtensions.IsNullOrEmpty<string>(null);
+
+      // ASSERT
+      Assert.IsTrue(result);
+    }
+
+    [Test]
+    public void IsNullOrEmpty_Should_Return_True_When_Source_Is_Empty()
+    {
+      // ACT
+      var result = Linq.Enumerable.Empty<string>().IsNullOrEmpty();
+
+      // ASSERT
+      Assert.IsTrue(result);
+    }
+
+    [Test]
+    public void IsNullOrEmpty_Should_Return_False_When_Source_Is_Not_Null_Or_Empty()
+    {
+      // ACT
+      var result = new [] { "test" }.IsNullOrEmpty();
+
+      // ASSERT
+      Assert.IsFalse(result);
+    }
+
+    [Test]
+    public void ForEach_Throws_When_Source_Is_Null()
+    {
+      // ACT
+      Assert.Throws<ArgumentNullException>(() => EnumerableExtensions.ForEach<string>(null, _ => { }));
+    }
+
+    [Test]
+    public void ForEach_Throws_When_ToBeDone_Is_Null()
+    {
+      // ARRANGE
+      var source = Linq.Enumerable.Empty<string>();
+
+      // ACT
+      Assert.Throws<ArgumentNullException>(() => source.ForEach(null));
+    }
+
+    [Test]
+    public void ForEach_Calls_ToBeDone_For_Each_Item_In_Source()
+    {
+      // ARRANGE
+      var source = new[] {"this", "is", "a", "test"};
+
+      // ACT
+      var strings = new List<string>();
+      source.ForEach(item => strings.Add(item));
+
+      // ASSERT
+      CollectionAssert.AreEqual(new List<string> {"this", "is", "a", "test"}, strings);
+    }
+  }
+}
+
