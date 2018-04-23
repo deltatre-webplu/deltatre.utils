1ae767529fee05d139d802d1640ea28f657190cf
Enrico Massone <enrico.massone@deltatre.it>
2018-04-23 10:29:23 +0200

    added test class for parsing result

--- tests/Deltatre.Utils.Tests/Dto/ParsingResultTest.cs
+++ tests/Deltatre.Utils.Tests/Dto/ParsingResultTest.cs
@@ -1,12 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Deltatre.Utils.Dto;
+using NUnit.Framework;
 
 namespace Deltatre.Utils.Tests.Dto
 {
-    class ParsingResultTest
-    {
-    }
+	[TestFixture]
+	public class ParsingResultTest
+	{
+		[Test]
+		public void CreateInvalid_Returns_An_Instance_Representing_Failed_Validation()
+		{
+			// ACT
+			var result = ParsingResult<string>.CreateInvalid();
+
+			// ASSERT
+			Assert.IsNotNull(result);
+			Assert.IsFalse(result.IsValid);
+			Assert.AreEqual(default(string), result.ParsedValue);
+		}
+
+		[Test]
+		public void CreateValid_Returns_An_Instance_Representing_Successful_Validation()
+		{
+			// ACT
+			var result = ParsingResult<string>.CreateValid("hello world");
+
+			// ASSERT
+			Assert.IsNotNull(result);
+			Assert.IsTrue(result.IsValid);
+			Assert.AreEqual("hello world", result.ParsedValue);
+		}
+	}
 }
