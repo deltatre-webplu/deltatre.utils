d0fdb9c4c164745c02bbe9001e334c3f320d3c6e
Enrico Massone <enrico.massone@deltatre.it>
2018-05-04 15:28:40 +0200

    Added class NormalizationResult<TValue, TError>

---
+++ src/Deltatre.Utils/Dto/NormalizationResult_WithError.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+
+namespace Deltatre.Utils.Dto
+{
+	/// <summary>
+	/// Represents the result obtained when normalizing a value of type TValue. Normalization errors are of type TError
+	/// </summary>
+	/// <typeparam name="TValue">The type of value being normalized</typeparam>
+	/// <typeparam name="TError">The type of normalization errors</typeparam>
+	public class NormalizationResult<TValue, TError>
+	{
+		/// <summary>
+		/// Call this method to create an instance representing the result of a failed normalization.
+		/// </summary>
+		/// <param name="errors">All the detected normalization errors.</param>
+		/// <exception cref="ArgumentNullException">Throws ArgumentNullException when parameter errors is null</exception>
+		/// <returns>An instance representing the result of a failed normalization.</returns>
+		/// <remarks>Property NormalizedValue will be set equal to the default value of type TValue.</remarks>
+		public static NormalizationResult<TValue, TError> CreateInvalid(IEnumerable<TError> errors)
+		{
+			if (errors == null)
+				throw new ArgumentNullException(nameof(errors));
+
+			return new NormalizationResult<TValue, TError>(false, default(TValue), errors);
+		}
+
+		/// <summary>
+		/// Call this method to create an instance representing the result of a successful normalization. The list of errors will be set to an empty list.
+		/// </summary>
+		/// <param name="normalizedValue">The result produced from the normalization process.</param>
+		/// <returns>An instance representing the result of a successful normalization.</returns>
+		public static NormalizationResult<TValue, TError> CreateValid(TValue normalizedValue) =>
+			new NormalizationResult<TValue, TError>(true, normalizedValue, Enumerable.Empty<TError>());
+
+		/// <summary>
+		/// Indicates whether the value that was normalized is valid 
+		/// </summary>
+		public bool IsValid { get; }
+
+		/// <summary>
+		/// This is the result produced from the normalization process. In case of failed normalization it will be set to the default value of type TValue.
+		/// </summary>
+		public TValue NormalizedValue { get; }
+
+		/// <summary>
+		/// All the detected normalization errors. In case of successfull normalization this collection will be empty.
+		/// </summary>
+		public ReadOnlyCollection<TError> Errors { get; }
+
+		private NormalizationResult(bool isValid, TValue normalizedValue, IEnumerable<TError> errors)
+		{
+			IsValid = isValid;
+			NormalizedValue = normalizedValue;
+			Errors = new ReadOnlyCollection<TError>(errors.ToList());
+		}
+	}
+}
