34ac126b594dbc67933e7158ca4f3fe747dd2154
Enrico Massone <enrico.massone@deltatre.it>
2018-05-07 12:38:26 +0200

    Implemented type NonEmptySequence

---
+++ tests/Deltatre.Utils.Tests/Types/NonEmptySequenceTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+using Deltatre.Utils.Types;
+using NUnit.Framework;
+
+namespace Deltatre.Utils.Tests.Types
+{
+	[TestFixture]
+	public class NonEmptySequenceTest
+	{
+		[Test]
+		public void Ctor_Throws_When_Items_Is_Null()
+		{
+			// ACT
+			Assert.Throws<ArgumentNullException>(() => new NonEmptySequence<string>(null));
+		}
+
+		[Test]
+		public void Ctor_Throws_When_Items_Is_Empty_Sequence()
+		{
+			// ACT
+			Assert.Throws<ArgumentException>(() => new NonEmptySequence<string>(Enumerable.Empty<string>()));
+		}
+
+		[Test]
+		public void Ctor_Creates_An_Enumerable_Instance()
+		{
+			// ARRANGE
+			var items = new[] {"foo", "bar"};
+
+			// ACT
+			var result = new NonEmptySequence<string>(items);
+
+			// ASSERT
+			Assert.IsNotNull(result);
+			CollectionAssert.AreEqual(new[] { "foo", "bar" }, result);
+		}
+	}
+}
