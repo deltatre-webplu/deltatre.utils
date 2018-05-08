2b35470189cad87d0a3278b3468cd22cc18d36fa
Enrico Massone <enrico.massone@deltatre.it>
2018-05-08 12:41:49 +0200

    Added interface ISystemClock useful for testing purposes

---
+++ src/Deltatre.Utils/Mocking/ISystemClock.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Deltatre.Utils.Mocking
+{
+	/// <summary>
+	/// This interface is an abstraction representing the system clock. It is useful to write testable classes relying on the system clock.
+	/// </summary>
+	public interface ISystemClock
+	{
+		/// <summary>
+		/// Represents the current date and time, expressed as the local time.
+		/// </summary>
+		DateTime Now { get; }
+
+		/// <summary>
+		/// Represents the current date and time, expressed as the Coordinated Universal Time (UTC).
+		/// </summary>
+		DateTime UtcNow { get; }
+	}
+}
