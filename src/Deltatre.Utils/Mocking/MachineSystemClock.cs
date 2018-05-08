035bbd027cdbef21afe6dcde9a0f0753bfddd00a
Enrico Massone <enrico.massone@deltatre.it>
2018-05-08 12:48:43 +0200

    Added class MachineSystemClock

---
+++ src/Deltatre.Utils/Mocking/MachineSystemClock.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Deltatre.Utils.Mocking
+{
+	/// <inheritdoc />
+	/// <summary>
+	/// Concrete implementation of ISystemClock based on your machine's system clock.
+	/// </summary>
+	public sealed class MachineSystemClock: ISystemClock
+	{
+		public DateTime Now => DateTime.Now;
+		public DateTime UtcNow => DateTime.UtcNow;
+	}
+}
