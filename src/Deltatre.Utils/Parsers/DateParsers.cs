883c58f42d20d601b1b694dbf4a3056bbd2b0444
Enrico Massone <enrico.massone@deltatre.it>
2018-05-18 18:44:17 +0200

    work in progress

---
+++ src/Deltatre.Utils/Parsers/DateParsers.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Globalization;
+using Deltatre.Utils.Dto;
+
+namespace Deltatre.Utils.Parsers
+{
+	public static class DateParsers
+	{
+		private static readonly string[]  Iso8601Formats = new string[]
+		{
+			//Microsoft standard date and time format strings
+			"s",
+			"o"
+		};
+
+		/// <summary>
+		/// Parses a date and time of day represented as a string in the format ISO 8601.
+		/// </summary>
+		/// <param name="toBeParsed">The ISO 8601 string to be parsed</param>
+		/// <returns>An object of type ParsingResult which represents the result of the performed parsing operation</returns>
+		/// <exception cref="ArgumentException">Throws ArgumentException if a null or white space string is passed in</exception>
+		public static ParsingResult<DateTimeOffset> ParseIso8601Date(string toBeParsed)
+		{
+			if(string.IsNullOrWhiteSpace(toBeParsed))
+				throw new ArgumentException($"Parameter '{toBeParsed}' cannot be null or white space.");
+
+			var isSuccess = DateTimeOffset.TryParseExact(
+				toBeParsed, 
+				Iso8601Formats, 
+				CultureInfo.InvariantCulture, 
+				DateTimeStyles.RoundtripKind,
+				out var result);
+
+			return ParsingResult<DateTimeOffset>.CreateValid(result);
+		}
+	}
+}
---
+++ tests/Deltatre.Utils.Tests/Parsers/DateParsersTest.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+using static Deltatre.Utils.Parsers.DateParsers;
+
+namespace Deltatre.Utils.Tests.Parsers
+{
+	[TestFixture]
+	public class DateParsersTest
+	{
+		[TestCase(null)]
+		[TestCase("")]
+		[TestCase("    ")]
+		public void ParseIso8601Date_Throws_When_Parameter_ToBeParsedIsNullOrWhiteSpace(string toBeParsed)
+		{
+			// ACT
+			Assert.Throws<ArgumentException>(() => ParseIso8601Date(toBeParsed));
+		}
+
+		// see https://docs.microsoft.com/en-us/dotnet/standard/base-types/standard-date-and-time-format-strings#Sortable
+		[Test]
+		public void ParseIso8601Date_Support_Microsoft_Sortable_Format_Specifier()
+		{
+			// ARRANGE
+			const string toBeParsed = "2008-04-10T06:30:00";
+
+			// ACT
+			var result = ParseIso8601Date(toBeParsed);
+			
+			// ASSERT
+			Assert.IsNotNull(result);
+			Assert.IsTrue(result.IsValid);
+			
+			// Check parsed value
+			var expected = new DateTimeOffset(new DateTime(2008, 4, 10, 6, 30, 0, DateTimeKind.Local));
+			Assert.AreEqual(expected, result.ParsedValue);
+		}
+
+		// see https://docs.microsoft.com/en-us/dotnet/standard/base-types/standard-date-and-time-format-strings#the-round-trip-o-o-format-specifier
+		[TestCaseSource(nameof(Microsoft_RoundTrip_Format_Test_Case_Source))]
+		public void ParseIso8601Date_Support_Microsoft_RoundTrip_Format_Specifier((string toBeParsed, DateTimeOffset expected) tuple)
+		{
+			// ACT
+			var result = ParseIso8601Date(tuple.toBeParsed);
+
+			// ASSERT
+			Assert.IsNotNull(result);
+			Assert.IsTrue(result.IsValid);
+			Assert.AreEqual(tuple.expected, result.ParsedValue);
+		}
+
+		private static IEnumerable<(string toBeParsed, DateTimeOffset expected)> Microsoft_RoundTrip_Format_Test_Case_Source()
+		{
+			yield return (
+				"2009-06-15T13:45:30.0080000",
+				new DateTimeOffset(new DateTime(2009, 6, 15, 13, 45, 30, 8, DateTimeKind.Unspecified)));
+
+			yield return (
+				"2009-06-15T13:45:30.0080000Z",
+				new DateTimeOffset(new DateTime(2009, 6, 15, 13, 45, 30, 8, DateTimeKind.Utc)));
+		}
+	}
+}
