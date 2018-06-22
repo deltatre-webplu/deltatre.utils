15e85c45faf287ef384eefda3da556ca530c7237
Enrico Massone <enrico.massone@deltatre.it>
2018-06-22 15:20:53 +0200

    Implemented RandomHelpers static class

---
+++ src/Deltatre.Utils/Randomization/RandomHelpers.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading;
+
+namespace Deltatre.Utils.Randomization
+{
+	/// <summary>
+	/// Use this class to get a thread local instance of Random class. Instances of different threads will have different seeds.
+	/// </summary>
+	public static class RandomHelpers
+	{
+		private static int _seed = Environment.TickCount;
+
+		private static readonly ThreadLocal<Random> ThreadLocalRandom = new ThreadLocal<Random>(() => 
+				new Random(Interlocked.Increment(ref _seed))
+		);
+
+		/// <summary>
+		/// The thread local instance of Random class.
+		/// </summary>
+		public static Random Instance => ThreadLocalRandom.Value;
+	}
+}
