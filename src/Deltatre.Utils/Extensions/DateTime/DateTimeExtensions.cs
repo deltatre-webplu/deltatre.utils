2640dec0a6a91ca237f28b17e08952f75430c07e
Giovanni Frigione <giovanni.frigione@deltatre.com>
2018-07-30 10:35:35 +0200

    Added DateTimeExtensions with a method to convert a DateTime to Unix Time

---
+++ src/Deltatre.Utils/Extensions/DateTime/DateTimeExtensions.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Deltatre.Utils.Extensions.DateTimeExtension
+{
+  /// <summary>
+  /// 
+  /// </summary>  
+  public static class DateTimeExtensions
+  {
+    private static DateTime _epoch;
+    /// <summary>
+    /// Unix Epoch Time (1970-01-01T00:00:00Z)
+    /// </summary>
+    public static DateTime Epoch
+    {
+      get
+      {
+        if(_epoch == default(DateTime))
+          _epoch = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+
+        return _epoch;
+      }
+    }
+
+    /// <summary>
+    /// Returns the number of seconds that have elapsed since Epoch (1970-01-01T00:00:00Z)
+    /// </summary>
+    /// <param name="date">DateTime to convert in Unix Time</param>
+    /// <returns>The number of seconds that have elapsed since Epoch (1970-01-01T00:00:00Z)</returns>
+    public static int ToUnixTimeSeconds(this DateTime date)
+    {
+      var utcDate = date.ToUniversalTime();
+      return (int) utcDate.Subtract(Epoch).TotalSeconds;
+    }
+  }
+}
---
+++ tests/Deltatre.Utils.Tests/Extensions/DateTime/DateTimeExtensionsTest.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Deltatre.Utils.Extensions.DateTimeExtension;
+using NUnit.Framework;
+
+namespace Deltatre.Utils.Tests.Extensions.DateTimeExtension
+{
+  [TestFixture]
+  public class DateTimeExtensionsTest
+  {
+    [Test]
+    public void Epoch_Should_Return_Unix_Epoch_Time()
+    {
+      // ARRANGE
+      var expectedEpoch = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+
+      // ACT
+      var actualEpoch = DateTimeExtensions.Epoch;
+
+      // ASSERT
+      Assert.AreEqual(expectedEpoch, actualEpoch);
+    }
+
+    [Test]
+    public void ToUnixTimeSeconds_Should_Return_Zero_When_Called_With_Epoch()
+    {
+      // ARRANGE
+      var epoch = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+
+      //ACT
+      var unixTimeSeconds = epoch.ToUnixTimeSeconds();
+
+      // ASSERT
+      Assert.AreEqual(0, unixTimeSeconds);
+    }
+
+    [Test]
+    public void ToUnixTimeSeconds_Should_Return_86400_When_Called_With_Epoch_Plus_One_Day()
+    {
+      // ARRANGE
+      var date = new DateTime(1970, 1, 2, 0, 0, 0, DateTimeKind.Utc);
+
+      //ACT
+      var unixTimeSeconds = date.ToUnixTimeSeconds();
+
+      // ASSERT
+      Assert.AreEqual(86400, unixTimeSeconds);
+    }
+
+    [Test]
+    public void ToUnixTimeSeconds_Should_Return_Minus_86400_When_Called_With_Epoch_Minus_One_Day()
+    {
+      // ARRANGE
+      var date = new DateTime(1969, 12, 31, 0, 0, 0, DateTimeKind.Utc);
+
+      //ACT
+      var unixTimeSeconds = date.ToUnixTimeSeconds();
+
+      // ASSERT
+      Assert.AreEqual(-86400, unixTimeSeconds);
+    }
+  }
+}
