f645c8f00035e5c3bd56a8dc82a82bf8879aa781
Enrico Massone <enrico.massone@deltatre.it>
2018-08-24 09:36:49 +0200

    introduced operation result with errors

---
+++ src/Deltatre.Utils/Dto/OperationResult_WithError.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using Deltatre.Utils.Types;
+
+namespace Deltatre.Utils.Dto
+{
+	/// <summary>
+	/// Represents the result obtained when performing an operation
+	/// </summary>
+	/// <typeparam name="TOutput">The type of the operation output</typeparam>
+	/// <typeparam name="TError">The type of operation errors</typeparam>
+	public sealed class OperationResult<TOutput, TError>
+	{
+		/// <summary>
+		/// Call this method to create an instance representing the result of a failed operation.
+		/// </summary>
+		/// <param name="errors">All the detected errors</param>
+		/// <exception cref="ArgumentNullException">Throws ArgumentNullException when parameter errors is null</exception>
+		/// <returns>An instance representing the result of a failed operation.</returns>
+		/// <remarks>Property Output will be set equal to the default value of type TOutput.</remarks>
+		public static OperationResult<TOutput, TError> CreateSuccess(NonEmptySequence<TError> errors)
+		{
+			if (errors == null)
+				throw new ArgumentNullException(nameof(errors));
+
+			return new OperationResult<TOutput, TError>(default(TOutput), errors);
+		}
+
+		/// <summary>
+		/// Call this method to create an instance representing the result of a successful operation. The list of errors will be set to an empty list.
+		/// </summary>
+		/// <param name="output">The operation output.</param>
+		/// <returns>An instance representing the result of a successful operation.</returns>
+		public static OperationResult<TOutput, TError> CreateValid(TOutput output) =>
+			new OperationResult<TOutput, TError>(output, Enumerable.Empty<TError>());
+
+		/// <summary>
+		/// Indicates whether the operation completed successfully. 
+		/// </summary>
+		public bool IsSuccess => Errors.Count == 0;
+
+		/// <summary>
+		/// This is the result produced from the operation. In case of failed operation it will be set to the default value of type TOutput.
+		/// </summary>
+		public TOutput Output { get; }
+
+		/// <summary>
+		/// All the detected errors. In case of successfull operation this collection will be empty.
+		/// </summary>
+		public ReadOnlyCollection<TError> Errors { get; }
+
+		private OperationResult(TOutput output, IEnumerable<TError> errors)
+		{
+			Output = output;
+			Errors = new ReadOnlyCollection<TError>(errors.ToList());
+		}
+	}
+}
