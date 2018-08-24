df4c79a67ab695ebbef70ebb9c4ac0f16fe5754b
Enrico Massone <enrico.massone@deltatre.it>
2018-08-24 09:29:30 +0200

    Introduces class operation result

---
+++ src/Deltatre.Utils/Dto/OperationResult.cs
@@ -0,0 +1,41 @@
+﻿namespace Deltatre.Utils.Dto
+{
+	/// <summary>
+	/// Represents the result obtained when performing an operation
+	/// </summary>
+	/// <typeparam name="TOutput">The type of the operation output</typeparam>
+	public sealed class OperationResult<TOutput>
+	{
+		/// <summary>
+		/// Call this method to create an instance representing the result of a successful operation.
+		/// </summary>
+		/// <param name="output">The operation output.</param>
+		/// <returns>An instance representing the result of a successful operation.</returns>
+		public static OperationResult<TOutput> CreateSuccess(TOutput output) =>
+			new OperationResult<TOutput>(true, output);
+
+		/// <summary>
+		/// Call this method to create an instance representing the result of a failed operation.
+		/// </summary>
+		/// <returns>An instance representing the result of a failed operation.</returns>
+		/// <remarks>Property Output will be set equal to the default value of type TOutput.</remarks>
+		public static OperationResult<TOutput> CreateFailure() =>
+			new OperationResult<TOutput>(false, default(TOutput));
+
+		/// <summary>
+		/// Indicates whether the operation completed successfully. 
+		/// </summary>
+		public bool IsSuccess { get; }
+
+		/// <summary>
+		/// This is the result produced from the operation. In case of failed operation it will be set to the default value of type TOutput.
+		/// </summary>
+		public TOutput Output { get; }
+
+		private OperationResult(bool isSuccess, TOutput output)
+		{
+			IsSuccess = isSuccess;
+			Output = output;
+		}
+	}
+}
