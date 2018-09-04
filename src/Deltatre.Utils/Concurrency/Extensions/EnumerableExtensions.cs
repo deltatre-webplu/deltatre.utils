fa67111f5716137b92f28f9674fc00ae50ceb6ac
Enrico Massone <enrico.massone@deltatre.it>
2018-09-05 00:23:24 +0200

    work in progress

--- src/Deltatre.Utils/Concurrency/Extensions/EnumerableExtensions.cs
+++ src/Deltatre.Utils/Concurrency/Extensions/EnumerableExtensions.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using System.Collections.Concurrent;
 using System.Linq;
+using System.IO;
 
 namespace Deltatre.Utils.Concurrency.Extensions
 {
@@ -52,6 +53,42 @@ namespace Deltatre.Utils.Concurrency.Extensions
 										}
 									});
 			return Task.WhenAll(tasks);
+		}
+
+		/// <summary>
+		/// Executes an asynchronous operation for each item inside a source sequence. These operations are run concurrently in a parallel fashion. The invokation returns a task whose result is a sequence containing the results of all the asynchronous operations (in source sequence order). It is possible to constrain the maximum number of parallel operations.
+		/// </summary>
+		/// <typeparam name="TSource">The type of the items inside the source sequence</typeparam>
+		/// <typeparam name="TResult">The type of the object produced by invoking the operation on any item of the source sequence</typeparam>
+		/// <param name="source">The source sequence</param>
+		/// <param name="maxDegreeOfParallelism">The maximum number of operations that are able to run in parallel</param>
+		/// <param name="operation">The asynchronous operation to be executed for each item inside the source sequence. This operation will produce a result of type <typeparamref name="TResult"/></param>
+		/// <returns>A task which completes when all of the asynchronous operations (one for each item inside the source sequence) complete. This task will produce a sequence of objects of type <typeparamref name="TResult"/></returns>
+		/// <exception cref="ArgumentNullException"><paramref name="source"/> is <c>null</c>.</exception>
+		/// <exception cref="ArgumentNullException"><paramref name="operation"/> is <c>null</c>.</exception>
+		/// <exception cref="ArgumentOutOfRangeException"><paramref name="maxDegreeOfParallelism"/> is less than or equal to zero.</exception>
+		public static IEnumerable<TResult> ForEachAsync<TSource, TResult>(
+			this IEnumerable<TSource> source,
+			int maxDegreeOfParallelism,
+			Func<TSource, Task<TResult>> operation)
+		{
+			if (source == null)
+				throw new ArgumentNullException(nameof(source));
+
+			if (operation == null)
+				throw new ArgumentNullException(nameof(operation));
+
+			if (maxDegreeOfParallelism <= 0)
+			{
+				throw new ArgumentOutOfRangeException(
+					nameof(maxDegreeOfParallelism),
+					$"Invalid value for the maximum degree of parallelism: {maxDegreeOfParallelism}. The maximum degree of parallelism must be a positive integer.");
+			}
+
+
+
+
+			throw new NotImplementedException();
 		}
 	}
 }
