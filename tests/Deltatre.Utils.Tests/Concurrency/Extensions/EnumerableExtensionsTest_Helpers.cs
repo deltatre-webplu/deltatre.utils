217eeb4920b43986ca75ac8bc53bab20006bd7a4
Enrico Massone <enrico.massone@deltatre.it>
2018-09-08 01:27:10 +0200

    Added two helpers methods in order to complete unit tests

---
+++ tests/Deltatre.Utils.Tests/Concurrency/Extensions/EnumerableExtensionsTest_Helpers.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Deltatre.Utils.Tests.Concurrency.Extensions
+{
+	public partial class EnumerableExtensionsTest
+	{
+		private static bool AreOverlapping(
+			(DateTime start, DateTime end) first,
+			(DateTime start, DateTime end) second)
+		{
+			return first.end > second.start && second.end > first.start;
+		}
+
+		private static T[] GetOthers<T>(T[] items, int index)
+		{
+			var numberOfElementsBefore = index;
+			var itemsToSkip = index + 1;
+
+			return items.Take(numberOfElementsBefore).Concat(items.Skip(itemsToSkip)).ToArray();
+		}
+	}
+}
