8f14ad2658c172bf93247244855d727aa11c5ab5
Enrico Massone <enrico.massone@deltatre.it>
2018-10-06 23:42:09 +0200

    implemented method

---
+++ src/Deltatre.Utils/Validators/UrlHelpers.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Deltatre.Utils.Validators
+{
+  /// <summary>
+  /// A collection of helper methods to validate URLs
+  /// </summary>
+  public static class UrlHelpers
+  {
+    /// <summary>
+    /// Checks whether a string is a valid absolute url. Both HTTP and HTTPS scheme are taken into account.
+    /// </summary>
+    /// <param name="value">The string to be validated as an absolute URL</param>
+    /// <returns>True if parameter <paramref name="value"/> is a valid absolute URL, false otherwise</returns>
+    public static bool IsValidAbsoluteUrl(string value)
+    {
+      if (string.IsNullOrWhiteSpace(value))
+        return false;
+
+      var isAbsoluteUrl = Uri.TryCreate(value, UriKind.Absolute, out var url);
+      if (!isAbsoluteUrl)
+        return false;
+
+      return url.Scheme == Uri.UriSchemeHttp || url.Scheme == Uri.UriSchemeHttps;
+    }
+  }
+}
---
+++ tests/Deltatre.Utils.Tests/Validators/UrlHelpersTest.cs
@@ -0,0 +1,64 @@
+﻿using Deltatre.Utils.Validators;
+using NUnit.Framework;
+
+namespace Deltatre.Utils.Tests.Validators
+{
+  [TestFixture]
+  public class UrlHelpersTest
+  {
+    [TestCase(null)]
+    [TestCase("")]
+    [TestCase("    ")]
+    public void IsValidAbsoluteUrl_Returns_False_If_Url_Is_Null_Or_White_Space(string url)
+    {
+      // ACT
+      var result = UrlHelpers.IsValidAbsoluteUrl(url);
+
+      // ASSERT
+      Assert.IsFalse(result);
+    }
+
+    [TestCase("http://contoso.it")]
+    [TestCase("http://contoso.it/")]
+    [TestCase("http://www.facebook.com")]
+    [TestCase("http://www.facebook.com/")]
+    [TestCase("http://www.facebook.com/foo")]
+    [TestCase("http://www.facebook.com/foo/")]
+    [TestCase("http://www.facebook.com/foo/bar")]
+    [TestCase("http://www.facebook.com/foo/bar/")]
+    [TestCase("http://www.facebook.com/foo/bar?key=value")]
+    [TestCase("http://www.facebook.com/foo/bar?key=value&pippo=pluto")]
+    [TestCase("http://www.facebook.com/foo/bar/?key=value")]
+    [TestCase("http://www.facebook.com/foo/bar/?key=value&pippo=pluto")]
+    public void IsValidAbsoluteUrl_Returns_True_If_Url_Is_Http_Absolute_Url(string url)
+    {
+      // ACT
+      var result = UrlHelpers.IsValidAbsoluteUrl(url);
+
+      // ASSERT
+      Assert.IsTrue(result);
+    }
+
+    [TestCase("https://contoso.it")]
+    [TestCase("https://contoso.it/")]
+    [TestCase("https://www.facebook.com")]
+    [TestCase("https://www.facebook.com/")]
+    [TestCase("https://www.facebook.com/foo")]
+    [TestCase("https://www.facebook.com/foo/")]
+    [TestCase("https://www.facebook.com/foo/bar")]
+    [TestCase("https://www.facebook.com/foo/bar/")]
+    [TestCase("https://www.facebook.com/foo/bar?key=value")]
+    [TestCase("https://www.facebook.com/foo/bar?key=value&pippo=pluto")]
+    [TestCase("https://www.facebook.com/foo/bar/?key=value")]
+    [TestCase("https://www.facebook.com/foo/bar/?key=value&pippo=pluto")]
+    public void IsValidAbsoluteUrl_Returns_True_If_Url_Is_Https_Absolute_Url(string url)
+    {
+      // ACT
+      var result = UrlHelpers.IsValidAbsoluteUrl(url);
+
+      // ASSERT
+      Assert.IsTrue(result);
+    }
+
+  }
+}
