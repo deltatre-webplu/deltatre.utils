fbda38b89a1d1bf1f6a31dc7d76bf983e3f79b3a
Enrico Massone <enrico.massone@deltatre.it>
2018-10-06 19:49:49 +0200

    work in progress

---
+++ src/Deltatre.Utils/Randomization/RandomHelpers.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Deltatre.Utils.Randomization
+{
+  /// <summary>
+  /// Helper methods for generating random objects
+  /// </summary>
+  public static class RandomHelpers
+  {
+    /// <summary>
+    /// Call this method to get a random alphanumeric string 
+    /// </summary>
+    /// <param name="length">The lenght of the random string to be generated</param>
+    /// <returns>A string having the desired lenght composed by alphanumeric characters randomly chosen</returns>
+    /// <exception cref="ArgumentOutOfRangeException">Throws <see cref="ArgumentOutOfRangeException"/> when parameter <paramref name="length"/> is less than zero</exception>
+    public static string GetRandomAlphanumericString(int length)
+    {
+      throw new NotImplementedException();
+    }
+  }
+}
---
+++ tests/Deltatre.Utils.Tests/Randomization/RandomHelpersTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Deltatre.Utils.Randomization;
+using NUnit.Framework;
+
+namespace Deltatre.Utils.Tests.Randomization
+{
+  [TestFixture]
+  public class RandomHelpersTest
+  {
+    [Test]
+    public void GetRandomAlphanumericString_Throws_Argument_Out_Of_Range_Exception_When_Lenght_Is_Less_Than_Zero()
+    {
+      // ACT
+      Assert.Throws<ArgumentOutOfRangeException>(() => RandomHelpers.GetRandomAlphanumericString(-3));
+    }
+
+    [Test]
+    public void GetRandomAlphanumericString_Returns_Empty_String_When_Length_Equals_Zero()
+    {
+      // ACT
+      var result = RandomHelpers.GetRandomAlphanumericString(0);
+
+      // ASSERT
+      Assert.IsNotNull(result);
+      Assert.AreEqual(string.Empty, result);
+    }
+  }
+}
