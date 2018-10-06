f62dbbce0745f8fb42363e88fed54cb27f6a0129
Enrico Massone <enrico.massone@deltatre.it>
2018-10-06 19:03:58 +0200

    added readonly static field for failure

--- src/Deltatre.Utils/Dto/OperationResult.cs
+++ src/Deltatre.Utils/Dto/OperationResult.cs
@@ -1,4 +1,6 @@
-﻿namespace Deltatre.Utils.Dto
+﻿using System;
+
+namespace Deltatre.Utils.Dto
 {
 	/// <summary>
 	/// Represents the result obtained when performing an operation
@@ -6,36 +8,42 @@
 	/// <typeparam name="TOutput">The type of the operation output</typeparam>
 	public sealed class OperationResult<TOutput>
 	{
-		/// <summary>
-		/// Call this method to create an instance representing the result of a successful operation.
-		/// </summary>
-		/// <param name="output">The operation output.</param>
-		/// <returns>An instance representing the result of a successful operation.</returns>
-		public static OperationResult<TOutput> CreateSuccess(TOutput output) =>
+    /// <summary>
+    /// The result of a failed operation
+    /// </summary>
+    public static readonly OperationResult<TOutput> Failure = new OperationResult<TOutput>(false, default(TOutput));
+
+    /// <summary>
+    /// Call this method to create an instance representing the result of a successful operation.
+    /// </summary>
+    /// <param name="output">The operation output.</param>
+    /// <returns>An instance representing the result of a successful operation.</returns>
+    public static OperationResult<TOutput> CreateSuccess(TOutput output) =>
 			new OperationResult<TOutput>(true, output);
 
-		/// <summary>
-		/// Call this method to create an instance representing the result of a failed operation.
-		/// </summary>
-		/// <returns>An instance representing the result of a failed operation.</returns>
-		/// <remarks>Property Output will be set equal to the default value of type TOutput.</remarks>
-		public static OperationResult<TOutput> CreateFailure() =>
+    /// <summary>
+    /// Call this method to create an instance representing the result of a failed operation.
+    /// </summary>
+    /// <returns>An instance representing the result of a failed operation.</returns>
+    /// <remarks>Property Output will be set equal to the default value of type TOutput.</remarks>
+    [Obsolete("Use the static field Failure instead")]
+    public static OperationResult<TOutput> CreateFailure() =>
 			new OperationResult<TOutput>(false, default(TOutput));
 
-		/// <summary>
-		/// Indicates whether the operation completed successfully. 
-		/// </summary>
-		public bool IsSuccess { get; }
+    private OperationResult(bool isSuccess, TOutput output)
+    {
+      IsSuccess = isSuccess;
+      Output = output;
+    }
+
+    /// <summary>
+    /// Indicates whether the operation completed successfully. 
+    /// </summary>
+    public bool IsSuccess { get; }
 
 		/// <summary>
 		/// This is the result produced from the operation. In case of failed operation it will be set to the default value of type TOutput.
 		/// </summary>
 		public TOutput Output { get; }
-
-		private OperationResult(bool isSuccess, TOutput output)
-		{
-			IsSuccess = isSuccess;
-			Output = output;
-		}
 	}
 }
