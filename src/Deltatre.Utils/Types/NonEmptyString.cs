81487a5c7923ec94f978645971ac954b1e2eab42
Enrico Massone <enrico.massone@deltatre.it>
2018-10-10 18:44:57 +0200

    Implemented non empty string type

---
+++ src/Deltatre.Utils/Types/NonEmptyString.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Deltatre.Utils.Types
+{
+  /// <summary>
+  /// This type wraps a string which is guaranteed to be neither null nor white space
+  /// </summary>
+  public sealed class NonEmptyString
+  {
+    /// <summary>
+    /// Implicit conversion from <see cref="NonEmptyString"/> to <see cref="string"/>
+    /// </summary>
+    /// <param name="nonEmptyString">The instance of <see cref="NonEmptyString"/> to be converted</param>
+    public static implicit operator string(NonEmptyString nonEmptyString)
+    {
+      return nonEmptyString?.Value;
+    }
+
+    /// <summary>
+    /// Explicit conversion from <see cref="string"/> to <see cref="NonEmptyString"/>
+    /// </summary>
+    /// <param name="value">The instance of <see cref="string"/> to be converted</param>
+    /// <exception cref="InvalidCastException">Throws <see cref="InvalidCastException"/> when <paramref name="value"/> is null or white space</exception>
+    public static explicit operator NonEmptyString(string value)
+    {
+      try
+      {
+        return new NonEmptyString(value);
+      }
+      catch (ArgumentException ex)
+      {
+        throw new InvalidCastException($"Unable to convert the provided string to {typeof(NonEmptyString).Name}", ex);
+      }
+    }
+
+    /// <summary>
+    /// Creates new instance of <see cref="NonEmptyString"/>
+    /// </summary>
+    /// <param name="value">The string to be wrapped</param>
+    /// <exception cref="ArgumentException">Throws <see cref="ArgumentException"/> when parameter <paramref name="value"/> is null or white space</exception>
+    public NonEmptyString(string value)
+    {
+      if (string.IsNullOrWhiteSpace(value))
+        throw new ArgumentException($"Parameter {nameof(value)} cannot be null or white space", nameof(value));
+
+      this.Value = value;
+    }
+
+    /// <summary>
+    /// Gets the wrapped string
+    /// </summary>
+    public string Value { get; }
+  }
+}
---
+++ tests/Deltatre.Utils.Tests/Types/NonEmptyStringTest.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using Deltatre.Utils.Types;
+using NUnit.Framework;
+
+namespace Deltatre.Utils.Tests.Types
+{
+  [TestFixture]
+  public class NonEmptyStringTest
+  {
+    [TestCase(null)]
+    [TestCase("")]
+    [TestCase("    ")]
+    public void Ctor_Throws_ArgumenException_When_Value_Is_Null_Or_White_Space(string value)
+    {
+      // ACT
+      Assert.Throws<ArgumentException>(() => new NonEmptyString(value));
+    }
+
+    [Test]
+    public void Ctor_Is_Able_To_Create_New_Instance()
+    {
+      // ACT
+      var result = new NonEmptyString("hello world");
+
+      // ASSERT
+      Assert.IsNotNull(result);
+      Assert.AreEqual("hello world", result.Value);
+    }
+
+    [Test]
+    public void Implicit_Cast_To_String_Is_Supported_From_Null_Instance()
+    {
+      // ARRANGE
+      NonEmptyString instance = null;
+
+      // ACT
+      string result = instance;
+
+      // ASSERT
+      Assert.IsNull(result);
+    }
+
+    [Test]
+    public void Implicit_Cast_To_String_Is_Supported_From_Non_Null_Instance()
+    {
+      // ARRANGE
+      var instance = new NonEmptyString("foo bar");
+
+      // ACT
+      string result = instance;
+
+      // ASSERT
+      Assert.AreEqual("foo bar", result);
+    }
+
+    [TestCase(null)]
+    [TestCase("")]
+    [TestCase("    ")]
+    public void Explicit_Cast_Throws_InvalidCastExcpetion_When_Value_Is_Null_Or_White_Space(string value)
+    {
+      // ACT
+      NonEmptyString result = null;
+      var exception = Assert.Throws<InvalidCastException>(() => result = (NonEmptyString)value);
+
+      // ASSERT
+      Assert.IsNotNull(exception);
+      Assert.IsNotNull(exception.InnerException);
+      Assert.IsInstanceOf<ArgumentException>(exception.InnerException);
+    }
+
+    [Test]
+    public void Explicit_Cast_From_String_Is_Available()
+    {
+      // ACT
+      const string value = "Test String";
+      NonEmptyString result = (NonEmptyString)value;
+
+      // ASSERT
+      Assert.IsNotNull(result);
+      Assert.AreEqual("Test String", result.Value);
+    }
+  }
+}
