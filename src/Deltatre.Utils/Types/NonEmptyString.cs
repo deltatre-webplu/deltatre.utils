84b103b35e5a5b89a01fcb1d32b4fcbf4369a5e2
Enrico Massone <enrico.massone@deltatre.it>
2018-10-11 11:34:48 +0200

    Changed NonEmptyString to be a struct with value based equality semantic

--- src/Deltatre.Utils/Types/NonEmptyString.cs
+++ src/Deltatre.Utils/Types/NonEmptyString.cs
@@ -5,7 +5,7 @@ namespace Deltatre.Utils.Types
   /// <summary>
   /// This type wraps a string which is guaranteed to be neither null nor white space
   /// </summary>
-  public sealed class NonEmptyString
+  public struct NonEmptyString
   {
     /// <summary>
     /// Implicit conversion from <see cref="NonEmptyString"/> to <see cref="string"/>
@@ -13,7 +13,7 @@ namespace Deltatre.Utils.Types
     /// <param name="nonEmptyString">The instance of <see cref="NonEmptyString"/> to be converted</param>
     public static implicit operator string(NonEmptyString nonEmptyString)
     {
-      return nonEmptyString?.Value;
+      return nonEmptyString.Value;
     }
 
     /// <summary>
@@ -50,5 +50,54 @@ namespace Deltatre.Utils.Types
     /// Gets the wrapped string
     /// </summary>
     public string Value { get; }
+
+    /// <summary>Indicates whether this instance and a specified object are equal.</summary>
+    /// <param name="obj">The object to compare with the current instance. </param>
+    /// <returns>
+    ///     <see langword="true" /> if <paramref name="obj" /> and this instance are the same type and represent the same value; otherwise, <see langword="false" />. </returns>
+    public override bool Equals(object obj)
+    {
+      if (!(obj is NonEmptyString))
+      {
+        return false;
+      }
+
+      var other = (NonEmptyString)obj;
+      return this.Value == other.Value;
+    }
+
+    /// <summary>Returns the hash code for this instance.</summary>
+    /// <returns>A 32-bit signed integer that is the hash code for this instance.</returns>
+    public override int GetHashCode()
+    {
+      unchecked
+      {
+        int hash = 17;
+        hash = (hash * 23) + (this.Value == null ? 0 : this.Value.GetHashCode());
+        return hash;
+      }
+    }
+
+    /// <summary>
+    /// Compares two instances of <see cref="NonEmptyString"/> for equality
+    /// </summary>
+    /// <param name="left">An instance of <see cref="NonEmptyString"/></param>
+    /// <param name="right">An instance of <see cref="NonEmptyString"/></param>
+    /// <returns></returns>
+    public static bool operator ==(NonEmptyString left, NonEmptyString right)
+    {
+      return left.Equals(right);
+    }
+
+    /// <summary>
+    /// Compares two instances of <see cref="NonEmptyString"/> for inequality
+    /// </summary>
+    /// <param name="left">An instance of <see cref="NonEmptyString"/></param>
+    /// <param name="right">An instance of <see cref="NonEmptyString"/></param>
+    /// <returns></returns>
+    public static bool operator !=(NonEmptyString left, NonEmptyString right)
+    {
+      return !(left == right);
+    }
   }
 }
--- tests/Deltatre.Utils.Tests/Types/NonEmptyStringTest.cs
+++ tests/Deltatre.Utils.Tests/Types/NonEmptyStringTest.cs
@@ -28,20 +28,7 @@ namespace Deltatre.Utils.Tests.Types
     }
 
     [Test]
-    public void Implicit_Cast_To_String_Is_Supported_From_Null_Instance()
-    {
-      // ARRANGE
-      NonEmptyString instance = null;
-
-      // ACT
-      string result = instance;
-
-      // ASSERT
-      Assert.IsNull(result);
-    }
-
-    [Test]
-    public void Implicit_Cast_To_String_Is_Supported_From_Non_Null_Instance()
+    public void Implicit_Cast_To_String_Is_Supported()
     {
       // ARRANGE
       var instance = new NonEmptyString("foo bar");
@@ -59,8 +46,10 @@ namespace Deltatre.Utils.Tests.Types
     public void Explicit_Cast_Throws_InvalidCastExcpetion_When_Value_Is_Null_Or_White_Space(string value)
     {
       // ACT
-      NonEmptyString result = null;
-      var exception = Assert.Throws<InvalidCastException>(() => result = (NonEmptyString)value);
+      var exception = Assert.Throws<InvalidCastException>(() =>
+      {
+        NonEmptyString result = (NonEmptyString)value;
+      });
 
       // ASSERT
       Assert.IsNotNull(exception);
@@ -79,5 +68,219 @@ namespace Deltatre.Utils.Tests.Types
       Assert.IsNotNull(result);
       Assert.AreEqual("Test String", result.Value);
     }
+
+    [Test]
+    public void Equals_Compares_Instances_By_Value()
+    {
+      // ARRANGE
+      var instance1 = new NonEmptyString("foo");
+      var instance2 = new NonEmptyString("foo");
+      var instance3 = new NonEmptyString("bar");
+      var instance4 = new NonEmptyString("FOO");
+      var instance5 = new NonEmptyString("fOo");
+
+      // ACT
+      var result1 = instance1.Equals(instance2);
+      var result2 = instance1.Equals(instance3);
+      var result3 = instance1.Equals(instance4);
+      var result4 = instance1.Equals(instance5);
+
+      // ASSERT
+      Assert.IsTrue(result1);
+      Assert.IsFalse(result2);
+      Assert.IsFalse(result3);
+      Assert.IsFalse(result4);
+    }
+
+    [Test]
+    public void Equals_Is_Reflexive()
+    {
+      // ARRANGE
+      var instance = new NonEmptyString("foo");
+
+      // ACT
+      var result = instance.Equals(instance);
+
+      // ASSERT
+      Assert.IsTrue(result);
+    }
+
+    [Test]
+    public void Equals_Is_Simmetric()
+    {
+      // ARRANGE
+      var left = new NonEmptyString("foo");
+      var right = new NonEmptyString("foo");
+
+      // ACT
+      var result1 = left.Equals(right);
+      var result2 = right.Equals(left);
+
+      // ASSERT
+      Assert.IsTrue(result1);
+      Assert.IsTrue(result2);
+    }
+
+    [Test]
+    public void Equals_Is_Transitive()
+    {
+      // ARRANGE
+      var instance1 = new NonEmptyString("foo");
+      var instance2 = new NonEmptyString("foo");
+      var instance3 = new NonEmptyString("foo");
+
+      // ACT
+      var result1 = instance1.Equals(instance2);
+      var result2 = instance2.Equals(instance3);
+      var result3 = instance1.Equals(instance3);
+
+      // ASSERT
+      Assert.IsTrue(result1);
+      Assert.IsTrue(result2);
+      Assert.IsTrue(result3);
+    }
+
+    [Test]
+    public void Equals_Returns_False_When_Obj_Is_Null()
+    {
+      // ARRANGE
+      var instance = new NonEmptyString("foo");
+
+      // ACT
+      var result = instance.Equals(null);
+
+      // ASSERT
+      Assert.IsFalse(result);
+    }
+
+    [Test]
+    public void Equals_Returns_False_When_Obj_Is_Not_Of_Type_NonEmptyString()
+    {
+      // ARRANGE
+      var instance = new NonEmptyString("foo");
+
+      // ACT
+      var result1 = instance.Equals(13);
+      var result2 = instance.Equals(13M);
+      var result3 = instance.Equals("foo");
+      var result4 = instance.Equals(new DateTime(2015, 1, 1));
+      var result5 = instance.Equals(new TestClass());
+      var result6 = instance.Equals(new TestStruct());
+
+      // ASSERT
+      Assert.IsFalse(result1);
+      Assert.IsFalse(result2);
+      Assert.IsFalse(result3);
+      Assert.IsFalse(result4);
+      Assert.IsFalse(result5);
+      Assert.IsFalse(result6);
+    }
+
+    [Test]
+    public void When_Two_Instances_Are_Equals_Then_They_Have_Same_Hash_Code()
+    {
+      // ARRANGE
+      var left = new NonEmptyString("foo");
+      var right = new NonEmptyString("foo");
+
+      // ACT
+      var result1 = left.GetHashCode();
+      var result2 = right.GetHashCode();
+
+      // ASSERT
+      Assert.AreEqual(result1, result2);
+    }
+
+    [Test]
+    public void Equal_Operator_Compares_Instances_By_Value()
+    {
+      // ARRANGE
+      var instance1 = new NonEmptyString("foo");
+      var instance2 = new NonEmptyString("foo");
+      var instance3 = new NonEmptyString("bar");
+      var instance4 = new NonEmptyString("FOO");
+      var instance5 = new NonEmptyString("fOo");
+
+      // ACT
+      var result1 = instance1 == instance2;
+      var result2 = instance1 == instance3;
+      var result3 = instance1 == instance4;
+      var result4 = instance1 == instance5;
+
+      // ASSERT
+      Assert.IsTrue(result1);
+      Assert.IsFalse(result2);
+      Assert.IsFalse(result3);
+      Assert.IsFalse(result4);
+    }
+
+    [Test]
+    public void Equal_Operator_Is_Reflexive()
+    {
+      // ARRANGE
+      var instance = new NonEmptyString("foo");
+
+      // ACT
+      var result = instance == instance;
+
+      // ASSERT
+      Assert.IsTrue(result);
+    }
+
+    [Test]
+    public void Equal_Operator_Is_Simmetric()
+    {
+      // ARRANGE
+      var left = new NonEmptyString("foo");
+      var right = new NonEmptyString("foo");
+
+      // ACT
+      var result1 = left == right;
+      var result2 = right == left;
+
+      // ASSERT
+      Assert.IsTrue(result1);
+      Assert.IsTrue(result2);
+    }
+
+    [Test]
+    public void Equal_Operator_Is_Transitive()
+    {
+      // ARRANGE
+      var instance1 = new NonEmptyString("foo");
+      var instance2 = new NonEmptyString("foo");
+      var instance3 = new NonEmptyString("foo");
+
+      // ACT
+      var result1 = instance1 == instance2;
+      var result2 = instance2 == instance3;
+      var result3 = instance1 == instance3;
+
+      // ASSERT
+      Assert.IsTrue(result1);
+      Assert.IsTrue(result2);
+      Assert.IsTrue(result3);
+    }
+
+    [Test]
+    public void NotEqual_Operator_Is_Coherent_With_Equal_Operator()
+    {
+      // ARRANGE
+      var instance1 = new NonEmptyString("foo");
+      var instance2 = new NonEmptyString("foo");
+      var instance3 = new NonEmptyString("bar");
+
+      // ACT
+      var result1 = instance1 != instance2;
+      var result2 = instance1 != instance3;
+
+      // ASSERT
+      Assert.IsFalse(result1);
+      Assert.IsTrue(result2);
+    }
+
+    public class TestClass { }
+
+    public struct TestStruct { }
   }
 }
