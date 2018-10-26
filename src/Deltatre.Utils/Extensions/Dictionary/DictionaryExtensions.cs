a1a96d418306bf82314f27a20e6c00dcff38cf5e
gianluca mangiapelo <gianluca.mangiapelo@deltatre.com>
2018-10-26 17:28:03 +0200

    Added utils extension method to get casted or default value from dictionary

--- src/Deltatre.Utils/Extensions/Dictionary/DictionaryExtensions.cs
+++ src/Deltatre.Utils/Extensions/Dictionary/DictionaryExtensions.cs
@@ -4,18 +4,49 @@ using System.Collections.ObjectModel;
 
 namespace Deltatre.Utils.Extensions.Dictionary
 {
-	/// <summary>
-	/// Extension methods for dictionaries
-	/// </summary>
-	public static class DictionaryExtensions
-	{
-		/// <exception cref="ArgumentNullException"> Throws ArgumentNullException when parameter source is null </exception>
-		public static ReadOnlyDictionary<TKey, TValue> AsReadOnly<TKey, TValue>(this IDictionary<TKey, TValue> source)
-		{
-			if (source == null)
-				throw new ArgumentNullException(nameof(source));
+  /// <summary>
+  /// Extension methods for dictionaries
+  /// </summary>
+  public static class DictionaryExtensions
+  {
+    /// <exception cref="ArgumentNullException"> Throws ArgumentNullException when parameter source is null </exception>
+    public static ReadOnlyDictionary<TKey, TValue> AsReadOnly<TKey, TValue>(this IDictionary<TKey, TValue> source)
+    {
+      if (source == null)
+        throw new ArgumentNullException(nameof(source));
 
-			return new ReadOnlyDictionary<TKey, TValue>(source);
-		}
-	}
+      return new ReadOnlyDictionary<TKey, TValue>(source);
+    }
+
+    public static string GetStringOrDefault(this IDictionary<string, object> fields, string propertyName)
+    {
+      return GetValueOrDefault<string>(fields, propertyName);
+    }
+
+    public static bool GetBoolOrDefault(this IDictionary<string, object> fields, string propertyName)
+    {
+      return GetValueOrDefault<bool>(fields, propertyName);
+    }
+
+    public static int GetIntOrDefault(this IDictionary<string, object> fields, string propertyName)
+    {
+      return GetValueOrDefault<int>(fields, propertyName);
+    }
+
+    public static T GetValueOrDefault<T>(this IDictionary<string, object> fields, string propertyName)
+    {
+      try
+      {
+        if (fields != null && (fields.ContainsKey(propertyName)))
+        {
+          return (T)Convert.ChangeType(fields[propertyName], typeof(T));
+        }
+        return default(T);
+      }
+      catch
+      {
+        return default(T);
+      }
+    }
+  }
 }
