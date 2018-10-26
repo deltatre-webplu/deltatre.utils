be4d74ae290ac8a81017af5373b3758e1ffa4cd3
Emanuele Tortolone <emanuele.tortolone@gmail.com>
2018-10-26 21:22:18 +0200

    Created StringExtensions class

---
+++ src/Deltatre.Utils/Extensions/String/StringExtensions.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Deltatre.Utils.Extensions.String
+{
+    /// <summary>
+	/// Extension methods for strings
+	/// </summary>
+    public static class StringExtensions
+    {
+    }
+}
