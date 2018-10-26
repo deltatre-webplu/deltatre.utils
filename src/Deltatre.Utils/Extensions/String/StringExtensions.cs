4619527b32b337ca506c0ca15109d44d67ba3a7b
Emanuele Tortolone <emanuele.tortolone@gmail.com>
2018-10-26 21:38:28 +0200

    Implemented StripTags of StringExtennsions class

--- src/Deltatre.Utils/Extensions/String/StringExtensions.cs
+++ src/Deltatre.Utils/Extensions/String/StringExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text.RegularExpressions;
 
 namespace Deltatre.Utils.Extensions.String
 {
@@ -7,5 +8,18 @@ namespace Deltatre.Utils.Extensions.String
 	/// </summary>
     public static class StringExtensions
     {
+        /// <summary>
+		/// Call this method to remove html tags from the given string source
+		/// </summary>
+		/// <param name="source">The string to be stripped</param>
+		/// <returns>A string without html tags</returns>
+        public static string StripTags(string source)
+        {
+            if (string.IsNullOrWhiteSpace(source))
+            {
+                return string.Empty;
+            }
+            return Regex.Replace(source, "<.*?>|&.*?;", string.Empty);
+        }
     }
 }
