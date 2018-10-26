679b49b010ff4c07c9e48ce97ff2728ddf5ca8ed
Emanuele Tortolone <emanuele.tortolone@gmail.com>
2018-10-26 22:30:12 +0200

    Update bad comments indentation

--- src/Deltatre.Utils/Extensions/String/StringExtensions.cs
+++ src/Deltatre.Utils/Extensions/String/StringExtensions.cs
@@ -4,15 +4,15 @@ using System.Text.RegularExpressions;
 namespace Deltatre.Utils.Extensions.String
 {
     /// <summary>
-	/// Extension methods for strings
-	/// </summary>
+    /// Extension methods for strings
+    /// </summary>
     public static class StringExtensions
     {
         /// <summary>
-		/// Call this method to remove html tags from the given string source
-		/// </summary>
-		/// <param name="source">The string to be stripped</param>
-		/// <returns>A string without html tags</returns>
+        /// Call this method to remove html tags from the given string source
+        /// </summary>
+        /// <param name="source">The string to be stripped</param>
+        /// <returns>A string without html tags</returns>
         public static string StripTags(string source)
         {
             if (string.IsNullOrWhiteSpace(source))
