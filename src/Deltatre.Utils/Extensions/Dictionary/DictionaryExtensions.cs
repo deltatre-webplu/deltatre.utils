16bfcf5109bcf18e1fdbce993ccf3236c6c6d613
DELTATRE\marco.antonino <marco.antonino@deltatre.it>
2018-10-27 12:19:22 +0200

    improved description for getstringordefault

--- src/Deltatre.Utils/Extensions/Dictionary/DictionaryExtensions.cs
+++ src/Deltatre.Utils/Extensions/Dictionary/DictionaryExtensions.cs
@@ -30,7 +30,7 @@ namespace Deltatre.Utils.Extensions.Dictionary
     /// <param name="source">A dictionary object</param>
     /// <param name="key">A key of the dictionary</param>
     /// <returns>The relative <c>string</c> value of <paramref name="key"/> or
-    /// <c>null</c> if <paramref name="source"/> is <c>null</c> or <paramref name="key"/> does not exist</returns>
+    /// <c>null</c> if <paramref name="source"/> is <c>null</c> or <paramref name="key"/> does not exist inside <paramref name="source"/>.</returns>
     public static string GetStringOrDefault(this IDictionary<string, object> source, string key)
     {
       return GetValueOrDefault<string>(source, key);
