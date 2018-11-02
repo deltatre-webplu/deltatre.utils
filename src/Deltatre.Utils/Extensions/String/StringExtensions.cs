2a5472651a8c767c8d90d7f95f1a972614b5133e
enrico.massone@deltatre.it <enrico.massone@deltatre.it>
2018-11-02 10:24:39 +0100

    executed little refactoring inside string extensions class. Add unit tests

--- src/Deltatre.Utils/Extensions/String/StringExtensions.cs
+++ src/Deltatre.Utils/Extensions/String/StringExtensions.cs
@@ -1,26 +1,29 @@
-﻿using System;
-using System.Text.RegularExpressions;
+﻿using System.Text.RegularExpressions;
 
 namespace Deltatre.Utils.Extensions.String
 {
+  /// <summary>
+  /// Extension methods for strings
+  /// </summary>
+  public static class StringExtensions
+  {
+    private static readonly Regex HtmlTagRegex = new Regex(@"<\/?[A-Za-z0-9]+[^>]*>", RegexOptions.Compiled);
+
     /// <summary>
-    /// Extension methods for strings
+    /// This method is meant to be a simplistic tool to remove HTML tags (both opening and closing) from a string. Only the opening and closing tags will be removed the tag content won't.
+    /// Opening tags not matching any closing tag will be removed.
+    /// Closing tags not matching any opening tag will be removed.
+    /// Encoded HTML tags won't be removed.
+    /// Don't use this method as a general purpose string sanitization tool especially in critical contexts: it is regular expression based and this approach is known not to be optimal to parse HTML.
     /// </summary>
-    public static class StringExtensions
+    /// <param name="source">The string to be sanitized from HTML tags</param>
+    /// <returns>The source string cleaned by all the HTML tags</returns>
+    public static string StripHtmlTags(this string source)
     {
-        /// <summary>
-        /// Call this method to simply remove html tags from the given string source.
-        /// To include literal html in the returned string you should encode it (e.g.: &lt;h1&gt; Title here &lt;/h1&gt;).
-        /// </summary>
-        /// <param name="source">The string to be stripped</param>
-        /// <returns>A string without html tags</returns>
-        public static string StripTags(string source)
-        {
-            if (string.IsNullOrWhiteSpace(source))
-            {
-                return string.Empty;
-            }
-            return Regex.Replace(source, "<.*?>", string.Empty);
-        }
+      if (string.IsNullOrWhiteSpace(source))
+        return source;
+
+      return HtmlTagRegex.Replace(source, string.Empty);
     }
+  }
 }
--- tests/Deltatre.Utils.Tests/Extensions/String/StringExtensionsTest.cs
+++ tests/Deltatre.Utils.Tests/Extensions/String/StringExtensionsTest.cs
@@ -1,69 +1,72 @@
-﻿using System;
-using System.Text.RegularExpressions;
+﻿using System.Text.RegularExpressions;
 using Deltatre.Utils.Extensions.String;
 using NUnit.Framework;
 
 namespace Deltatre.Utils.Tests.Extensions.String
 {
-    [TestFixture]
-    class StringExtensionsTest
+  [TestFixture]
+  public class StringExtensionsTest
+  {
+    [Test]
+    public void StripHtmlTags_Returns_Null_For_Null_Source()
     {
-        [Test]
-        public void StripTags_Should_Return_Empty_String_For_Null_Source()
-        {
-            // ARRANGE
-            var expectedString = string.Empty;
+      // ACT
+      var result = StringExtensions.StripHtmlTags(null);
 
-            // ACT
-            var actualString = StringExtensions.StripTags(null);
-
-            // ASSERT
-            Assert.AreEqual(expectedString, actualString);
-        }
-
-        [Test]
-        public void StripTags_Should_Return_Empty_String_For_Empty_Source()
-        {
-            // ARRANGE
-            var expectedString = string.Empty;
-
-            // ACT
-            var actualString = StringExtensions.StripTags(string.Empty);
-
-            // ASSERT
-            Assert.AreEqual(expectedString, actualString);
-        }
-
-        [Test]
-        public void StripTags_Should_Return_Given_String_Without_Html_Tags()
-        {
-            string input = "This <b>is a bold tag</b> and this <span>is a span tag</span>";
-
-            // ARRANGE
-            string expectedString = "This is a bold tag and this is a span tag";
-
-            // ACT
-            string actualString = StringExtensions.StripTags(input);
-
-            // ASSERT
-            Assert.IsFalse(Regex.IsMatch(actualString, "<(.|\n)*?>"));
-            Assert.AreEqual(expectedString, actualString);
-        }
-
-        [Test]
-        public void StripTags_Should_Return_Given_String_Without_Html_Tags_Leaving_Escaped_Html()
-        {
-            string input = "<p>When you want to include <b>literal html</b> inside a web page you should encode it (e.g.: &lt;h1&gt; Title here &lt;/h1&gt;)</p>";
-
-            // ARRANGE
-            string expectedString = "When you want to include literal html inside a web page you should encode it (e.g.: &lt;h1&gt; Title here &lt;/h1&gt;)";
-
-            // ACT
-            string actualString = StringExtensions.StripTags(input);
-
-            // ASSERT
-            Assert.IsFalse(Regex.IsMatch(actualString, "<(.|\n)*?>"));
-            Assert.AreEqual(expectedString, actualString);
-        }
+      // ASSERT
+      Assert.IsNull(result);
     }
+
+    [Test]
+    public void StripHtmlTags_Returns_Empty_String_For_Empty_String_Source()
+    {
+      // ACT
+      var result = string.Empty.StripHtmlTags();
+
+      // ASSERT
+      Assert.IsNotNull(result);
+      Assert.IsEmpty(result);
+    }
+
+    [Test]
+    public void StripHtmlTags_Returns_White_Space_String_For_White_Space_String_Source()
+    {
+      // ACT
+      var result = StringExtensions.StripHtmlTags("   ");
+
+      // ASSERT
+      Assert.IsNotNull(result);
+      Assert.AreEqual("   ", result);
+    }
+
+    [Test]
+    public void StripHtmlTags_Should_Return_Given_String_Without_Html_Tags()
+    {
+      // ARRANGE
+      const string source = "This <b>is a bold tag</b> and this <span>is a span tag</span>";
+
+      // ACT
+      var result = source.StripHtmlTags();
+
+      // ASSERT
+      Assert.IsNotNull(result);
+      const string expectedString = "This is a bold tag and this is a span tag";
+      Assert.AreEqual(expectedString, result);
+    }
+
+    [Test]
+    public void StripHtmlTags_Should_Return_Given_String_Without_Html_Tags_Leaving_Escaped_Html()
+    {
+      // ARRANGE
+      const string source = "<p>When you want to include <b>literal html</b> inside a web page you should encode it (e.g.: &lt;h1&gt; Title here &lt;/h1&gt;)</p>";
+
+      // ACT
+      var result = source.StripHtmlTags();
+
+      // ASSERT
+      Assert.IsNotNull(result);
+      const string expectedString = "When you want to include literal html inside a web page you should encode it (e.g.: &lt;h1&gt; Title here &lt;/h1&gt;)";
+      Assert.AreEqual(expectedString, result);
+    }
+  }
 }
