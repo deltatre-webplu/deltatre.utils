bf71becf58ac06a5a1bb1e729268b22d86c28b62
Glub0t <gianluca.mangiapelo@gmail.com>
2018-11-16 10:02:12 +0100

    Added dictionary extension methods to get typed value  (#1)
    * Added utils extension method to get casted or default value from dictionary
    
    * Added properly unit test suite for dictionary extensions method
    
    * added xml documentation for GetValueOrDefault<T>
    
    * added xml documentation for AsReadOnly
    
    * refactroing of GetStringOrDefault
    
    * added xml documentation for GetStringOrDefault
    
    * done refactoring for GetBoolOrDefault and GetIntOrDefault
    
    * replaced thw word dictionary with paramref source
    
    * added xml doc for GetBoolOrDefault
    
    * improved description for GetValueOrDefault
    
    * improved description for GetStringOrDefault
    
    * added xml documentation for GetIntOrDefault
    
    * improved description for getstringordefault
    
    * created GetValueOrDefault_Should_Throw_When_Source_Is_Null
    
    * implemented new beahviour for GetValueOrDefault in order to pass GetValueOrDefault_Should_Throw_When_Source_Is_Null
    
    * GetValueOrDefault_Should_Throw_When_Source_Is_Null passed
    
    * updated methods description after GetValueOrDefault update
    
    * Add support for IFormatProvider in conversion stage
    
    * Update tests for IFormatProvider
    
    * Update tests with right inner function to be called

--- src/Deltatre.Utils/Extensions/Dictionary/DictionaryExtensions.cs
+++ src/Deltatre.Utils/Extensions/Dictionary/DictionaryExtensions.cs
@@ -1,21 +1,173 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Globalization;
 
 namespace Deltatre.Utils.Extensions.Dictionary
 {
-	/// <summary>
-	/// Extension methods for dictionaries
-	/// </summary>
-	public static class DictionaryExtensions
-	{
-		/// <exception cref="ArgumentNullException"> Throws ArgumentNullException when parameter source is null </exception>
-		public static ReadOnlyDictionary<TKey, TValue> AsReadOnly<TKey, TValue>(this IDictionary<TKey, TValue> source)
-		{
-			if (source == null)
-				throw new ArgumentNullException(nameof(source));
+  /// <summary>
+  /// Extension methods for dictionaries
+  /// </summary>
+  public static class DictionaryExtensions
+  {
+    ///<summary>Returns a read-only version of the <paramref name="source"/> dictionary</summary>
+    ///<param name="source">A dictionary object</param>
+    ///<returns cref="System.Collections.ObjectModel.ReadOnlyDictionary{TKey, TValue}">
+    ///A read-only version of <paramref name="source"/>
+    ///</returns>
+    /// <exception cref="ArgumentNullException"> Throws ArgumentNullException when parameter source is null </exception>
+    public static ReadOnlyDictionary<TKey, TValue> AsReadOnly<TKey, TValue>(this IDictionary<TKey, TValue> source)
+    {
+      if (source == null)
+        throw new ArgumentNullException(nameof(source));
 
-			return new ReadOnlyDictionary<TKey, TValue>(source);
-		}
-	}
+      return new ReadOnlyDictionary<TKey, TValue>(source);
+    }
+
+    /// <summary>
+    /// Returns the relative <c>string</c> value of <paramref name="key"/> or
+    /// <c>null</c> if <paramref name="source"/> is <c>null</c> or <paramref name="key"/> does not exist inside <paramref name="source"/>.
+    /// </summary>
+    /// <param name="source">A dictionary object</param>
+    /// <param name="key">A key of the dictionary</param>
+    /// <returns>The relative <c>string</c> value of <paramref name="key"/> or
+    /// <c>null</c> if <paramref name="key"/> does not exist inside <paramref name="source"/>.</returns>
+    /// <exception cref="ArgumentNullException"> Throws ArgumentNullException when parameter source is null </exception>
+    public static string GetStringOrDefault(this IDictionary<string, object> source, string key)
+    {
+      return GetValueOrDefault<string>(source, key);
+    }
+
+
+    /// <summary>
+    /// Returns the relative <c>string</c> value of <paramref name="key"/> or
+    /// <c>null</c> if <paramref name="source"/> is <c>null</c> or <paramref name="key"/> does not exist inside <paramref name="source"/>.
+    /// </summary>
+    /// <param name="source">A dictionary object</param>
+    /// <param name="key">A key of the dictionary</param>
+    /// <param name="provider">Format provider to customize the culture specific aspects of the type conversion</param>
+    /// <returns>The relative <c>string</c> value of <paramref name="key"/> or
+    /// <c>null</c> if <paramref name="key"/> does not exist inside <paramref name="source"/>.</returns>
+    /// <exception cref="ArgumentNullException"> Throws ArgumentNullException when parameter source is null </exception>
+    public static string GetStringOrDefault(this IDictionary<string, object> source, string key, IFormatProvider provider)
+    {
+      return GetValueOrDefault<string>(source, key, provider);
+    }
+
+
+    /// <summary>
+    /// Returns the relative <c>bool</c> value of <paramref name="key"/> or
+    /// <c>false</c> if <paramref name="source"/> is <c>null</c> or <paramref name="key"/> does not exist inside <paramref name="source"/>.
+    /// </summary>
+    /// <param name="source">A dictionary object</param>
+    /// <param name="key">A key of the dictionary</param>
+    /// <returns>
+    /// The relative <c>bool</c> value of <paramref name="key"/> or
+    /// <c>false</c> if <paramref name="key"/> does not exist
+    /// </returns>
+    /// <exception cref="ArgumentNullException"> Throws ArgumentNullException when parameter source is null </exception>
+    public static bool GetBoolOrDefault(this IDictionary<string, object> source, string key)
+    {
+      return GetValueOrDefault<bool>(source, key);
+    }
+
+
+    /// <summary>
+    /// Returns the relative <c>bool</c> value of <paramref name="key"/> or
+    /// <c>false</c> if <paramref name="source"/> is <c>null</c> or <paramref name="key"/> does not exist inside <paramref name="source"/>.
+    /// </summary>
+    /// <param name="source">A dictionary object</param>
+    /// <param name="key">A key of the dictionary</param>
+    /// <param name="provider">Format provider to customize the culture specific aspects of the type conversion</param>
+    /// <returns>
+    /// The relative <c>bool</c> value of <paramref name="key"/> or
+    /// <c>false</c> if <paramref name="key"/> does not exist
+    /// </returns>
+    /// <exception cref="ArgumentNullException"> Throws ArgumentNullException when parameter source is null </exception>
+    public static bool GetBoolOrDefault(this IDictionary<string, object> source, string key, IFormatProvider provider)
+    {
+      return GetValueOrDefault<bool>(source, key, provider);
+    }
+
+
+    /// <summary>
+    /// Returns the relative <c>int</c> value of <paramref name="key"/> or
+    /// 0 if <paramref name="source"/> is <c>null</c> or <paramref name="key"/> does not exist inside <paramref name="source"/>.
+    /// </summary>
+    /// <param name="source">A dictionary object</param>
+    /// <param name="key">A key of the dictionary</param>
+    /// <returns>The relative <c>int</c> value of <paramref name="key"/> or
+    /// 0 if <paramref name="key"/> does not exist inside <paramref name="source"/>.</returns>
+    /// <exception cref="ArgumentNullException"> Throws ArgumentNullException when parameter source is null </exception>
+    public static int GetIntOrDefault(this IDictionary<string, object> source, string key)
+    {
+      return GetValueOrDefault<int>(source, key);
+    }
+
+    /// <summary>
+    /// <c>Returns</c> the relative value of <paramref name="key"/> or
+    /// its default value for the type <typeparamref name="T"/> <c>if</c> <paramref name="source"/> is <c>null</c> or the key does not exist inside <paramref name="source"/>.
+    /// </summary>
+    /// <typeparam name="T">The desired type of the returning value</typeparam>
+    /// <param name="source">A Dictionary object</param>
+    /// <param name="key">A key of the dictionary</param>
+    /// <returns>
+    /// <para>The relative value of <paramref name="key"/> or</para>
+    /// <para>its default value if the key does not exist inside <paramref name="source"/>.</para>
+    /// </returns>
+    /// <exception cref="ArgumentNullException"> Throws ArgumentNullException when parameter source is null </exception>
+    public static T GetValueOrDefault<T>(this IDictionary<string, object> source, string key)
+    {
+      if (source == null)
+      {
+        throw new ArgumentNullException("The source dictionary is null");
+      }
+      try
+      {
+        if (source.ContainsKey(key))
+        {
+          return (T)Convert.ChangeType(source[key], typeof(T));
+        }
+        return default(T);
+      }
+      catch
+      {
+        return default(T);
+      }
+    }
+
+    /// <summary>
+    /// <c>Returns</c> the relative value of <paramref name="key"/> or
+    /// its default value for the type <typeparamref name="T"/> <c>if</c> <paramref name="source"/> is <c>null</c> or the key does not exist inside <paramref name="source"/>.
+    /// </summary>
+    /// <typeparam name="T">The desired type of the returning value</typeparam>
+    /// <param name="source">A Dictionary object</param>
+    /// <param name="key">A key of the dictionary</param>
+    /// <param name="provider"></param>
+    /// <returns>
+    /// <para>The relative value of <paramref name="key"/> or</para>
+    /// <para>its default value if the key does not exist inside <paramref name="source"/>.</para>
+    /// </returns>
+    /// <exception cref="ArgumentNullException"> Throws ArgumentNullException when parameter source is null </exception>
+    public static T GetValueOrDefault<T>(this IDictionary<string, object> source, string key, IFormatProvider provider)
+    {
+      if (source == null)
+      {
+        throw new ArgumentNullException("The source dictionary is null");
+      }
+      try
+      {
+        if (source.ContainsKey(key))
+        {
+          return (T)Convert.ChangeType(source[key], typeof(T), provider);
+        }
+        return default(T);
+      }
+      catch
+      {
+        return default(T);
+      }
+    }
+
+  }
 }
--- tests/Deltatre.Utils.Tests/Extensions/Dictionary/DictionaryExtensionsTest.cs
+++ tests/Deltatre.Utils.Tests/Extensions/Dictionary/DictionaryExtensionsTest.cs
@@ -1,41 +1,305 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using Deltatre.Utils.Extensions.Dictionary;
 using NUnit.Framework;
 
 namespace Deltatre.Utils.Tests.Extensions.Dictionary
 {
-	[TestFixture]
-	public class DictionaryExtensionsTest
-	{
-		[Test]
-		public void AsReadOnly_Throws_When_Source_Is_Null()
-		{
-			// ACT
-			Assert.Throws<ArgumentNullException>(() => DictionaryExtensions.AsReadOnly<string, object>(null));
-		}
+  [TestFixture]
+  public class DictionaryExtensionsTest
+  {
+    [Test]
+    public void AsReadOnly_Throws_When_Source_Is_Null()
+    {
+      // ACT
+      Assert.Throws<ArgumentNullException>(() => DictionaryExtensions.AsReadOnly<string, object>(null));
+    }
 
-		[Test]
-		public void AsReadOnly_Wraps_Source_In_ReadOnlyDictionary()
-		{
-			// ARRABGE
-			var source = new Dictionary<string, int>
-			{
-				["Foo"] = 10,
-				["Bar"] = 45
-			};
+    [Test]
+    public void AsReadOnly_Wraps_Source_In_ReadOnlyDictionary()
+    {
+      // ARRANGE
+      var source = new Dictionary<string, int>
+      {
+        ["Foo"] = 10,
+        ["Bar"] = 45
+      };
 
-			// ACT
-			var result = source.AsReadOnly();
+      // ACT
+      var result = source.AsReadOnly();
 
-			// ASSERT
-			Assert.IsNotNull(result);
-			Assert.AreEqual(2, result.Count);
-			Assert.IsTrue(result.ContainsKey("Foo"));
-			Assert.IsTrue(result.ContainsKey("Bar"));
-			Assert.AreEqual(10, result["Foo"]);
-			Assert.AreEqual(45, result["Bar"]);
-		}
-	}
+      // ASSERT
+      Assert.IsNotNull(result);
+      Assert.AreEqual(2, result.Count);
+      Assert.IsTrue(result.ContainsKey("Foo"));
+      Assert.IsTrue(result.ContainsKey("Bar"));
+      Assert.AreEqual(10, result["Foo"]);
+      Assert.AreEqual(45, result["Bar"]);
+    }
+
+    [Test]
+    public void GetValueOrDefault_Should_Throw_When_Source_Is_Null()
+    {
+      // ACT
+      Assert.Throws<ArgumentNullException>(() => DictionaryExtensions.GetValueOrDefault<object>(null, "key"));
+    }
+
+    [Test]
+    public void GetValueOrDefault_Should_Return_Specified_String_Type()
+    {
+      var source = new Dictionary<string, object>
+      {
+        ["stringKey"] = "stringValue"
+      };
+
+      var result = source.GetValueOrDefault<string>("stringKey");
+      var resultType = result.GetType();
+
+      Assert.AreEqual(resultType, typeof(string));
+    }
+
+    [Test]
+    public void GetValueOrDefault_Should_Return_Specified_Int_Type()
+    {
+      var source = new Dictionary<string, object>
+      {
+        ["IntKey"] = 2
+      };
+
+      var result = source.GetValueOrDefault<int>("intKey");
+      var resultType = result.GetType();
+
+      Assert.AreEqual(resultType, typeof(int));
+    }
+
+    [Test]
+    public void GetValueOrDefault_Should_Return_Specified_Bool_Type()
+    {
+      var source = new Dictionary<string, object>
+      {
+        ["boolKey"] = true
+      };
+
+      var result = source.GetValueOrDefault<bool>("boolKey");
+      var resultType = result.GetType();
+
+      Assert.AreEqual(resultType, typeof(bool));
+    }
+
+    [Test]
+    public void GetValueOrDefault_Should_Return_Default_Type_If_Not_Able_To_Cast_Type()
+    {
+      var source = new Dictionary<string, object>
+      {
+        ["boolKey"] = true
+      };
+
+      var result = source.GetValueOrDefault<int>("boolKey");
+      var resultType = result.GetType();
+
+      Assert.AreEqual(resultType, typeof(int));
+    }
+
+    [Test]
+    public void GetValueOrDefault_Should_Return_Catch_Default_Value_If_Not_Able_To_Cast_Type()
+    {
+      var source = new Dictionary<string, object>
+      {
+        ["stringKey"] = "stringValue"
+      };
+
+      var result = source.GetValueOrDefault<int>("stringKey");
+      var resultType = result.GetType();
+
+      Assert.AreEqual(resultType, typeof(int));
+    }
+
+    [Test]
+    public void GetValueOrDefault_Should_Return_Default_Type_If_Key_Is_Not_Found()
+    {
+      var source = new Dictionary<string, object>
+      {
+        ["boolKey"] = true
+      };
+
+      var result = source.GetValueOrDefault<int>("noFoundKey");
+      var resultType = result.GetType();
+
+      Assert.AreEqual(resultType, typeof(int));
+    }
+
+    [Test]
+    public void GetValueOrDefault_Should_Return_Default_Object_Type()
+    {
+      var source = new Dictionary<string, object>
+      {
+        ["boolKey"] = new object()
+      };
+
+      var result = source.GetValueOrDefault<object>("boolKey");
+      var resultType = result.GetType();
+
+      Assert.AreEqual(resultType, typeof(object));
+    }
+
+    [Test]
+    public void GetValueOrDefault_Should_Return_Default_Type_For_Custom_Class()
+    {
+      var source = new Dictionary<string, object>
+      {
+        ["customKey"] = new ObjectDisposedException("x")
+      };
+
+      var result = source.GetValueOrDefault<ObjectDisposedException>("customKey");
+      var resultType = result.GetType();
+
+      Assert.AreEqual(resultType, typeof(ObjectDisposedException));
+    }
+
+    [Test]
+    public void GetValueOrDefault_Should_Return_DateTime_Culture_Info_Based()
+    {
+      var source = new Dictionary<string, object>
+      {
+        ["dateKey"] = "8/18/2010"
+      };
+
+      var culture = new CultureInfo("en-US");
+      var result = source.GetValueOrDefault<DateTime>("dateKey",culture);
+      var expectedDate = new DateTime(2010, 8, 18);
+
+      Assert.AreEqual(result.Date, expectedDate.Date);
+    }
+
+    [Test]
+    public void GetValueOrDefault_Should_Return_Default_DateTime_If_No_CultureInfo_Conversion_Is_Possible()
+    {
+      var source = new Dictionary<string, object>
+      {
+        ["dateKey"] = "8/18/2010"
+      };
+
+      var culture = new CultureInfo("it-IT");
+      var result = source.GetValueOrDefault<DateTime>("dateKey", culture);
+      var notExpectedDate = new DateTime(2010, 8, 18);
+      var expectedDate = new DateTime(0001, 1, 1).Date;
+
+      Assert.AreNotEqual(result.Date, notExpectedDate.Date);
+      Assert.AreEqual(result.Date, expectedDate);
+    }
+
+
+    [Test]
+    public void GetValueOrDefault_Should_FallBack_On_Default_Value_If_Provider_Is_Null()
+    {
+      var source = new Dictionary<string, object>
+      {
+        ["dateKey"] = "16/7/2018"
+      };
+
+      CultureInfo culture = null;
+      var result = source.GetValueOrDefault<DateTime>("dateKey", culture);
+      var notExpectedDate = new DateTime(2018, 7, 16);
+      var expectedDate = new DateTime(0001, 1, 1).Date;
+
+      Assert.AreNotEqual(result.Date, notExpectedDate.Date);
+      Assert.AreEqual(result.Date, expectedDate);
+    }
+
+    [Test]
+    public void GetStringOrDefault_Should_Return_The_Expected_String_Value()
+    {
+      var source = new Dictionary<string, object>
+      {
+        ["stringKey"] = "valueString"
+      };
+
+      var result = source.GetStringOrDefault("stringKey");
+
+      Assert.AreEqual(result, "valueString");
+    }
+
+    [Test]
+    public void GetStringOrDefault_Should_Return_The_Default_String_Value()
+    {
+      var source = new Dictionary<string, object>
+      {
+        ["stringKey"] = "valueString"
+      };
+
+      var result = source.GetStringOrDefault("stringKeyNotFound");
+
+      Assert.AreEqual(result, null);
+    }
+
+    [Test]
+    public void GetStringOrDefault_Should_Return_String_CultureInfo_Based()
+    {
+
+      var source = new Dictionary<string, object>
+      {
+        ["doubleKey"] = 16325.62901
+      };
+
+      
+      var culture = CultureInfo.CreateSpecificCulture("de-DE");
+      var result = source.GetStringOrDefault("doubleKey", culture);
+
+      Assert.AreEqual("16325,62901",result);
+    }
+
+    [Test]
+    public void GetBoolOrDefault_Should_Return_The_Expected_Bool_Value()
+    {
+      var source = new Dictionary<string, object>
+      {
+        ["boolKey"] = true
+      };
+
+      var result = source.GetBoolOrDefault("boolKey");
+
+      Assert.AreEqual(result, true);
+    }
+
+    [Test]
+    public void GetBoolOrDefault_Should_Return_The_Default_Bool_Value()
+    {
+      var source = new Dictionary<string, object>
+      {
+        ["boolKey"] = true
+      };
+
+      var result = source.GetBoolOrDefault("boolKeyNotFound");
+
+      Assert.AreEqual(result, false);
+    }
+
+
+    [Test]
+    public void GetIntOrDefault_Should_Return_The_Expected_Int_Value()
+    {
+      var source = new Dictionary<string, object>
+      {
+        ["intKey"] = 10
+      };
+
+      var result = source.GetIntOrDefault("intKey");
+
+      Assert.AreEqual(result, 10);
+    }
+
+    [Test]
+    public void GetIntOrDefault_Should_Return_The_Default_Int_Value()
+    {
+      var source = new Dictionary<string, object>
+      {
+        ["intKey"] = 15
+      };
+
+      var result = source.GetIntOrDefault("intKeyNotFound");
+
+      Assert.AreEqual(result, 0);
+    }
+  }
 }
-
