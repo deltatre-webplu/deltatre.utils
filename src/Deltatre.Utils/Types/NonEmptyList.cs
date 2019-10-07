b6f92e37c22630995234091d7c735cee8cdf8709
Enrico Massone <enrico.massone@deltatre.it>
2019-10-07 18:05:17 +0200

    work in progress addding class NonEmptyList<T>

--- src/Deltatre.Utils/Types/NonEmptyList.cs
+++ src/Deltatre.Utils/Types/NonEmptyList.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Deltatre.Utils.Types
 {
@@ -11,18 +12,40 @@ namespace Deltatre.Utils.Types
   /// <typeparam name="T">The type of items contained into the sequence</typeparam>
   public sealed class NonEmptyList<T> : IReadOnlyList<T>
   {
-    public T this[int index] => throw new NotImplementedException();
+    private readonly List<T> _items;
 
-    public int Count => throw new NotImplementedException();
-
-    public IEnumerator<T> GetEnumerator()
+    /// <summary>
+    /// Initializes a new instance of the <see cref="NonEmptyList{T}"/> class.
+    /// </summary>
+    /// <param name="items">The items contained into the newly created instance of <see cref="NonEmptyList{T}"/>.</param>
+    /// <exception cref="ArgumentNullException">Throws <see cref="ArgumentNullException"/> when <paramref name="items"/> is null</exception>
+    /// <exception cref="ArgumentException">Throws <see cref="ArgumentException"/> when <paramref name="items"/> is an empty sequence</exception>
+    public NonEmptyList(IEnumerable<T> items)
     {
-      throw new NotImplementedException();
+      if (items == null)
+        throw new ArgumentNullException(nameof(items));
+
+      var itemsList = items.ToList();
+
+      if (itemsList.Count == 0)
+        throw new ArgumentException($"Parameter '{nameof(items)}' cannot be an empty sequence.");
+
+      _items = itemsList;
     }
 
-    IEnumerator IEnumerable.GetEnumerator()
-    {
-      throw new NotImplementedException();
-    }
+    /// <summary>Gets the element at the specified index in the read-only list.</summary>
+    /// <param name="index">The zero-based index of the element to get.</param>
+    /// <returns>The element at the specified index in the read-only list.</returns>
+    public T this[int index] => _items[index];
+
+
+    /// <summary>
+    /// Gets the number of elements in the collection.
+    /// </summary>
+    public int Count => _items.Count;
+
+    IEnumerator<T> IEnumerable<T>.GetEnumerator() => ((IEnumerable<T>)_items).GetEnumerator();
+
+    IEnumerator IEnumerable.GetEnumerator() => ((IEnumerable)_items).GetEnumerator();
   }
 }
--- src/Deltatre.Utils/Types/NonEmptySequence.cs
+++ src/Deltatre.Utils/Types/NonEmptySequence.cs
@@ -17,8 +17,9 @@ namespace Deltatre.Utils.Types
     /// <summary>
     /// Initializes a new instance of the <see cref="NonEmptySequence{T}"/> class.
     /// </summary>
-    /// <exception cref="ArgumentNullException">Throws ArgumentNullException when <paramref name="items"/> is null</exception>
-    /// <exception cref="ArgumentException">Throws ArgumentException when <paramref name="items"/> is an empty sequence</exception>
+    /// <param name="items">The items contained into the newly created instance of <see cref="NonEmptySequence{T}"/>.</param>
+    /// <exception cref="ArgumentNullException">Throws <see cref="ArgumentNullException"/> when <paramref name="items"/> is null</exception>
+    /// <exception cref="ArgumentException">Throws <see cref="ArgumentException"/> when <paramref name="items"/> is an empty sequence</exception>
     public NonEmptySequence(IEnumerable<T> items)
 		{
 			if (items == null)
