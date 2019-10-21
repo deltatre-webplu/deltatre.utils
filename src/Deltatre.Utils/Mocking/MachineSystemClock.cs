3abce09569cd788e699626788c38ee21736e5a8b
Enrico Massone <enrico.massone@deltatre.it>
2019-10-21 17:39:13 +0200

    Added members for DateTimeOffset.Now and DateTimeOffset.UtcNow to interface ISystemClock

--- src/Deltatre.Utils/Mocking/MachineSystemClock.cs
+++ src/Deltatre.Utils/Mocking/MachineSystemClock.cs
@@ -2,7 +2,6 @@
 
 namespace Deltatre.Utils.Mocking
 {
-	/// <inheritdoc />
 	/// <summary>
 	/// Concrete implementation of ISystemClock based on your machine's system clock.
 	/// </summary>
@@ -17,5 +16,17 @@ namespace Deltatre.Utils.Mocking
 		/// The current date and time on this computer expressed as UTC time
 		/// </summary>
 		public DateTime UtcNow => DateTime.UtcNow;
-	}
+
+    /// <summary>
+    /// Gets a <see cref="DateTimeOffset" /> object that is set to the current date and time on the current computer, 
+    /// with the offset set to the local time's offset from Coordinated Universal Time (UTC).
+    /// </summary>
+    public DateTimeOffset OffsetNow => DateTimeOffset.Now;
+
+    /// <summary>
+    /// Gets a <see cref="DateTimeOffset" /> object whose date and time are set to the current Coordinated Universal Time (UTC) date and time 
+    /// and whose offset is set to <see cref="TimeSpan.Zero" />
+    /// </summary>
+    public DateTimeOffset OffsetUtcNow => DateTimeOffset.UtcNow;
+  }
 }
